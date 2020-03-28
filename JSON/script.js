56f0d17c4e716417c9325935dbaa17f44a17584e
Timur681 <teekirienkov@gmail.com>
2020-03-28 13:03:32 +0300

    json v1

---
+++ JSON/script.js
@@ -0,0 +1,12 @@
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+};
+
+
+console.log(JSON.stringify(options));
