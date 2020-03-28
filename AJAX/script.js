919dd41813b7e0708774be3e9ed7a44fcae4fc6a
Timur681 <teekirienkov@gmail.com>
2020-03-28 23:02:39 +0300

    Создан конвертер валют

--- AJAX/script.js
+++ AJAX/script.js
@@ -3,3 +3,27 @@
 
 const inputRub = document.querySelector('.inputRUB'),
     inputUsd = document.querySelector('.inputUSD');
+
+
+
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    // request.open('GET', url, async, login, pass);
+
+    request.open('GET', 'current.json');   // 1 параметр - тип запроса (get - получение, post - отправка)
+                                           // 2 параметр - то с чем работаем
+    request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+
+    request.send();
+
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200) {         // Если request равен 4 статусу
+            let data = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / data.usd;
+        }
+    });
+});
