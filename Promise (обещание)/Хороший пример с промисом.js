84ff3a0abe3e484ed6e2fb6193a7d7fea390e23b
Timur681 <teekirienkov@gmail.com>
2020-03-31 17:43:51 +0300

    vladilen minin Promise

--- Promise (обещание)/Хороший пример с промисом.js
+++ Promise (обещание)/Хороший пример с промисом.js
@@ -1,39 +1,19 @@
-let drink = 1;
+console.log('Request data...');
 
 
-function shoot(arrow) {
-    console.log('Вы сделали выстрел...');
+setTimeout(()=>{
+    console.log('Preparing data...');
 
-    let promise = new Promise(function(resolve, reject){
-        setTimeout(function(){
-            Math.random() > .5 ? resolve({}) : reject('Вы промахнулись');
-        }, 3000);
-    });
+    const backendData = {
+        server: 'aws',
+        port: 2000,
+        status: 'working'
+    };
 
-
-    return promise;
-}
-
-
-function win() {
-    console.log('Вы победили!');
-    (drink == 1) ? buyBeer() : giveMoney();
-}
-
-function buyBeer() {
-    console.log('Вам купили пиво');
-}
-
-function giveMoney() {
-    console.log('Вам заплатили');
-}
-
-function loose() {
-    console.log('Вы проиграли!');
-}
+    setTimeout(() => {
+        backendData.modified = true;
+        console.log('Data received', backendData);
+    }, 2000);
 
 
-shoot({})
-        .then(mark => console.log('Вы попали в цель!')
-        .then(win)
-        .catch(loose);
+}, 2000);
