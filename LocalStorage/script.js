0e3692f87db03bfd7003bf512070ff0ec7b3d4bd
Timur681 <teekirienkov@gmail.com>
2020-04-06 17:48:26 +0300

    checkbox work add

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,14 +1,14 @@
-// Сохранение данных. Первый параметр - ключ, второй параметр - значение.
-localStorage.setItem('hay', 'hi');
+// // Сохранение данных. Первый параметр - ключ, второй параметр - значение.
+// localStorage.setItem('hay', 'hi');
 
-// Получение значения ключа
-console.log(localStorage.getItem('hay'));
+// // Получение значения ключа
+// console.log(localStorage.getItem('hay'));
 
-// Удаление ключе и его значения
-localStorage.removeItem('hey');
+// // Удаление ключе и его значения
+// localStorage.removeItem('hey');
 
-// Удаление всех ключей
-localStorage.clear();
+// // Удаление всех ключей
+// localStorage.clear();
 
 
 /* Практика
@@ -17,13 +17,16 @@ localStorage.clear();
 */
 window.addEventListener('DOMContentLoaded', function() {
 
+    // Получение чекбокса
     let checkbox = document.getElementById('rememberMe');
   
+    // Проверка, если в localStorage сохранен ключ со значением true
+    // то при загрузке страницы значение так же ставится true 
     if (localStorage.getItem('isChecked') === 'true') {
         checkbox.checked = true;
     }
 
-
+    // Если юзер ставит галочку, это значение сохраняется в localStorage
     checkbox.addEventListener('click', function() {
         localStorage.setItem('isChecked', true);
     });
