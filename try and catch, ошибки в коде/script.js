edd2d085c564cf9bfcdb9e3527cb80ef806a04bb
Timur681 <teekirienkov@gmail.com>
2020-04-06 19:25:51 +0300

    finally

--- try and catch, ошибки в коде/script.js
+++ try and catch, ошибки в коде/script.js
@@ -1,8 +1,25 @@
 // try/catch
 
+// Главная суть этой конструкции в том
+// что с помощью неё можно проверить какую-то определенную часть кода
+
+let json = '{"id":2}';
+
 try {
-    console.log('normal');
-} catch {
-    console.log('error');
+    let user = JSON.parse(json);
+    console.log(user);
+
+    // Создание своей ошибки с помощью оператора throw
+    // Если в user нет name, выводится новая ошибка
+    if (!user.name) {
+        throw new Error('Имени юзера нет!');
+    }
+
+} catch(error) {
+    console.log(error.name); // выводится название ошибки
+    console.log(error.message); // сообщение ошибки
+    console.log(error.stack); // ошибка полностью 
+
+    console.log(`Мы получили ошибку: ${error.name}`);
 }
 
