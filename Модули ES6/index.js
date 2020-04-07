9326e4e0b7df4b54a4e77b7a48e3786f2cee3b76
Timur681 <teekirienkov@gmail.com>
2020-04-07 15:55:35 +0300

    add modules

---
+++ Модули ES6/index.js
@@ -0,0 +1,8 @@
+// import
+
+
+import {one, two} from './script';
+
+
+
+console.log(one);
---
+++ Модули ES6/script.js
@@ -0,0 +1,13 @@
+// Mодули
+
+// Export
+
+export let one = 1;
+
+let two = 2;
+export {two};
+
+
+export function sayHi() {
+    console.log('Hello');
+}
