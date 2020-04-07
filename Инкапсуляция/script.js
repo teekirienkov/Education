896255d888377ff23cae0cce93c2e39de6af6547
Timur681 <teekirienkov@gmail.com>
2020-04-07 15:20:53 +0300

    add Инкапсуляция

--- Инкапсуляция/script.js
+++ Инкапсуляция/script.js
@@ -3,11 +3,25 @@
 
 function User(name, age) {
     this.name = name;
-    this.age = age;
+    // this.age = age;
 
+    let userAge = age; // переменная инкапсуляции
 
     this.say = function() {
-        console.log(`Имя пользователя ${this.name}, возраст: ${this.age}`);
+        console.log(`Имя пользователя ${this.name}, возраст: ${userAge}`);
+    };
+
+
+    this.getAge = function() {
+        return userAge;
+    };
+
+    this.setAge = function(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            userAge = age;
+        } else {
+            console.log('Error. Недопустимое значение.');
+        }
     };
 }
 
@@ -17,8 +31,13 @@ let ivan = new User('Ivan', 25);
 
 
 console.log(ivan.name);
-console.log(ivan.age);
 
 
-ivan.age = 30;
+console.log(ivan.getAge());
 
+
+ivan.setAge(30);
+
+console.log(ivan.getAge());
+
+ivan.say();
