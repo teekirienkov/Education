88bf1d1254edd20b5072617d95884a7098dbb925
Timur681 <teekirienkov@gmail.com>
2020-04-30 21:57:00 +0300

    переписал функцию с промисом, теперь она универсальная

--- XHR + Fetch/script.js
+++ XHR + Fetch/script.js
@@ -1,18 +1,30 @@
 const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
 
-const XHR = new XMLHttpRequest;
+function sendRequest (method, url) {
+	return new Promise((resolve, reject) => { // Возвращаем промис из функции
+		const XHR = new XMLHttpRequest;
 
-XHR.open('GET', urlUsers); // Метод запроса и ссылка
+		XHR.open(method, url); // Метод запроса и ссылка
 
-XHR.responseType = 'json'; // Указываем, то что нам нужно распарсить в json
+		XHR.responseType = 'json'; // Указываем, то что нам нужно распарсить в json
 
-XHR.onload = () => {
-	console.log(XHR.response); // Выводим ответ в консоль
+		XHR.onload = () => {
+			if (XHR.status >= 400) {
+				PromiseRejectionEvent(XHR.response);
+			} else {
+				resolve(XHR.response);
+			}
+		}
+
+		XHR.onerror = () => {
+			reject(xhr.response); // Указываем ошибку в ответе (если она есть)
+		}
+
+		XHR.send(); // Отправляем запрос
+	})
 }
 
-XHR.onerror = () => {
-	console.log(xhr.response); // Указываем ошибку в ответе (если она есть)
-}
-
-XHR.send(); // Отправляем запрос
+sendRequest('GET', urlUsers)
+	.then(data=>console.log(data))
+	.catch(err=>console.log(err))
