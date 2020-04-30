0b11836610259487391676b2133f76a34f8beaeb
Timur681 <teekirienkov@gmail.com>
2020-04-30 21:49:09 +0300

    add comment

--- XHR + Fetch/script.js
+++ XHR + Fetch/script.js
@@ -1,9 +1,18 @@
 const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
 
-
 const XHR = new XMLHttpRequest;
 
-XHR.open('GET', urlUsers);
+XHR.open('GET', urlUsers); // Метод запроса и ссылка
 
-XHR.send();
+XHR.responseType = 'json'; // Указываем, то что нам нужно распарсить в json
+
+XHR.onload = () => {
+	console.log(XHR.response); // Выводим ответ в консоль
+}
+
+XHR.onerror = () => {
+	console.log(xhr.response); // Указываем ошибку в ответе (если она есть)
+}
+
+XHR.send(); // Отправляем запрос
