2dea0c4601e71cbea154bd9d626cce80a39dba4f
Timur681 <teekirienkov@gmail.com>
2020-05-01 21:29:04 +0300

    add os module

---
+++ _Node JS/Introduction/NodeJsModules/os.js
@@ -0,0 +1,9 @@
+const os = require('os')
+
+console.log('Операционная система:', os.platform())
+console.log('Архитектура процессора:', os.arch())
+console.log('Инфа по процессорам:', os.cpus())
+console.log('Свободная память:', os.freemem())
+console.log('Всего памяти:', os.totalmem())
+console.log('Домашняя директория:', os.homedir())
+console.log('ПК включен:', os.uptime())
