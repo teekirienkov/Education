d1c9a3d9e6c96818913205db366a49c2e5fbccd1
Timur681 <teekirienkov@gmail.com>
2020-05-01 22:20:24 +0300

    add http server

--- _Node JS/Introduction/index.js
+++ _Node JS/Introduction/index.js
@@ -1,7 +1,69 @@
-const chalk = require('chalk');
-const logText = require('./module');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-logText('Hi world!');
+const server = http.createServer((request, response) => {
+	// if (request.url === '/') {
+	// 	fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+	// 		if (err) {
+	// 			throw err
+	// 		}
+	// 		// Аналог setRequestHeader в native JS
+	// 		// 1 параметр - status code, 2 параметр - headers
+	// 		response.writeHead(200, {
+	// 			'Content-Type': 'text/html'
+	// 		})
+	// 		response.end(data)
+	// 	})
+	// } else if (request.url === '/contact') {
+	// 	fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+	// 		if (err) {
+	// 			throw err
+	// 		}
+	// 		// Аналог setRequestHeader в native JS
+	// 		// 1 параметр - status code, 2 параметр - headers
+	// 		response.writeHead(200, {
+	// 			'Content-Type': 'text/html'
+	// 		})
+	// 		response.end(data)
+	// 	})
+	// }
+
+	let filePath = path.join(__dirname, 'public', request.url ==='/' ? 'index.html' : request.url)
+
+	// Получение расширения файла
+	const ext = path.extname(filePath)
+	// Если расширения нет, то добавляется .html
+	if (!ext) {
+		filePath += '.html'
+	}
 
 
-console.log(chalk.red('text test'));
+	// Этот способ почему-то не работает, сервер падает при попытке подключиться на страницы
+	
+	fs.writeFile(filePath, (err, content) => {
+		if (err) {
+			fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+				if (err) {
+					response.writeHead(500)
+					response.end('Error')
+				} else {
+					response.writeHead(200, {
+						'Content-Type': 'text/html'
+					})
+					response.end(data)
+				}
+			})
+		} else {
+			response.writeHead(200, {
+				'Content-Type': 'text/html'
+			})
+			response.end(content)
+		}
+	})
+})
+
+// Запуск сервера, первый параметр порт
+server.listen(3000, () => {
+	console.log('Server started!')
+})
