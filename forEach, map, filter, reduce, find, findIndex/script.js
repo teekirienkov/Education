6dc8c6bf91331dc94f7c169493b50fc4c40654da
Timur <teekirienkov@gmail.com>
2020-05-05 17:49:08 +0300

    add arrays methods dir

---
+++ forEach, map, filter, reduce, find, findIndex/script.js
@@ -0,0 +1,7 @@
+const people = [
+	{ name: 'Timur', age: '19', budget: '50000'},
+	{ name: 'Oleg', age: '45', budget: '7550000'},
+	{ name: 'Mihail', age: '22', budget: '150000'},
+	{ name: 'Alex', age: '37', budget: '30000'},
+	{ name: 'Vladilen', age: '25', budget: '250000'}
+]
