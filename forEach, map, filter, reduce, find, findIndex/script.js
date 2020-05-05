aebac86a999a242f63417184b2c108eac72903b1
Timur <teekirienkov@gmail.com>
2020-05-05 18:02:54 +0300

    map

--- forEach, map, filter, reduce, find, findIndex/script.js
+++ forEach, map, filter, reduce, find, findIndex/script.js
@@ -9,11 +9,19 @@ const people = [
 // for (let i = 0; i < people.length; i++) {
 // 	console.log(people[i])
 // }
+
 // forEach, принимает следующие параметры в функции - (item, index, arr)
 people.forEach((item) => {   // 1) item - это каждый элемент в массиве, 2) index - индекс, 3) arr - весь массив
-	console.log(item.age);
+	console.log(item.age); // 19, 45, 22 и т.д.
 })
+
 // Map
+// С помощью этого метода можно как угодно трансформировать данные 
+// и заносить их в новый массив newPeople
+const newPeople = people.map((person) => {
+	return person;
+})
+console.log(newPeople) // array people
 
 // Filter
 
