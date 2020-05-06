a111d42eb5c81e43367b116741eb64ce6227d147
Timur <teekirienkov@gmail.com>
2020-05-06 19:13:52 +0300

    add comments

--- Algorithms/Introduction/README.md
+++ Algorithms/Introduction/README.md
@@ -1,3 +1,5 @@
 <h1>Общие понятия в алгоритмах</h1>
 
 <p>Алгоритмом называется набор инструкций для выполнения некоторой задачи.</p>
+
+<a href="https://github.com/teekirienkov/EducationJS/blob/master/Algorithms/Introduction/binarySearch.js">Бинарный поиск</a>
--- Algorithms/Introduction/binarySearch.js
+++ Algorithms/Introduction/binarySearch.js
@@ -10,29 +10,28 @@ let low,   // low - начало массива
 		high,  // high - конец массива
 		guess;
 		
-
 function binarySearch(list, item) {
 	low = 0,   
-	high = list.length - 1;
+	high = list.length - 1; // конец массива
 
-	while (low <= high) {
-		mid = Math.floor((low + high) / 2); // округление в меньшую сторону
+	while (low <= high) {                 // Пока эта часть не сократится до одного элемента...
+		mid = Math.floor((low + high) / 2); // ...проверяем средний элемент
 		guess = list[mid];
 		
-		if (guess == item) {
-			return mid;
+		if (guess == item) {							  // Значение найдено,
+			return mid;												// останавливаем функцию и возвращаем mid
 		}
 
-		if (guess > item) {
+		if (guess > item) {									// Много
 			high = mid - 1;
-		} else {
+		} else {														// Мало
 			low = mid + 1;
 		}
 	}
-	return null;
+	return null;													// Значение не существует
 }
 
 const array = [1, 3, 5, 7, 9];
 		
-
-console.log(binarySearch(array, 3));
+console.log(binarySearch(array, 3)); // ищем 3 - получаем индекс массива 1
+console.log(binarySearch(array, 9)); // ищем 9 - получаем индекс массива 4
